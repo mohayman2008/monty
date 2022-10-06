33768d5441b83d756f6523ec3fe96d2a826bdcfb
Mohamed Ayman <mohayman2007@gmail.com>
2022-10-06 11:13:01 +0000

    Project 0x19 - Task 5 - "nop"

---
+++ exec.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: stack or queue head
+ * @line_number: number of the line starting from 1
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
